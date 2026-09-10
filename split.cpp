8790e5cfffe9d2d7f34b7f7597be81cb43257b17
Christina Abraham <cvabraha@usc.edu>
2026-09-10 06:23:08 +0000

    Completed split function part 1. No failed cases.

--- split.cpp
+++ split.cpp
@@ -16,8 +16,24 @@ the function below should be the only one in this file.
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  // base case
+  if (in == nullptr){
+    return;
+  }
+
+  Node* nextNode = in->next;
+
+  if(in->value %2 ==0){
+    evens = in;
+    evens->next = nullptr;
+    split(nextNode, odds, evens->next);
+  } else{
+    odds = in;
+    odds->next = nullptr;
+    split(nextNode, odds->next, evens);
+  }
+
+  in = nullptr;
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,50 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+
+void printList(Node* head){
+  while(head != NULL){
+    std :: cout << head->value << "->";
+    head = head->next;
+  }
+  std :: cout << "nullptr\n";
+}
+
+void deleteList(Node* head){
+  while(head != NULL){
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+  }
+}
 
 int main(int argc, char* argv[])
 {
+  Node* in = new Node(1, new Node(1, new Node(3, new Node(4, new Node(5, nullptr)))));
+  
+  Node* odds = nullptr;
+  Node* evens = nullptr;
 
+  std :: cout << "Original list before split: ";
+
+  split(in, odds, evens);
+
+  std :: cout << "Odds list: ";
+  printList(odds);
+
+  std :: cout << "Evens list: ";
+  printList(evens);
+
+  std :: cout << "List after split: ";
+
+  if(in == nullptr){
+    std :: cout << "nullptr\n";
+  } else{
+    std :: cout << "NOT nullptr\n";
+  }
+
+deleteList(odds);
+deleteList(evens);
 }
