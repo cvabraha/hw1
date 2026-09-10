f1da91732c8ed1fb6e13f8e79dda3f5e520f0a07
Christina Abraham <cvabraha@usc.edu>
2026-09-10 21:25:53 +0000

    all done with part 2. no failed tests or leaks.

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,43 @@
 
 int main(int argc, char* argv[])
 {
+  ULListStr dat;
+
+  std::cout << "1. Testing basic pushes: " << std::endl;
+  dat.push_back("apple");
+  dat.push_back("banana");
+  dat.push_front("zebra");
+  dat.push_front("yak");
+
+  std::cout << "List size: " << dat.size() << " (Expected: 4)" << std::endl;
+  std::cout << "Front: " << dat.front() << " (Expected: yak)" << std::endl;
+  std::cout << "Back: " << dat.back() << " (Expected: banana)" << std::endl;
+
+  std::cout << "\n2. Testing get(): " << std::endl;
+
+  for(size_t i = 0; i< dat.size(); i++){
+    std::cout << dat.get(i) << " ";
+  }
+
+  std::cout << std::endl;
+
+  std::cout << "\n3. Testing pops: ";
+  dat.pop_back();
+  dat.pop_front();
+
+  std::cout << "\n4. Is list empty? " << (dat.empty() ? "Yes" : "No") << std::endl;
+
+  std::cout << "\n5. Testing pops: ";
+  dat.pop_back();
+  dat.pop_front();
+
+  for (int i = 1; i <= 15; i++) {
+      dat.push_back(std::to_string(i));
+  }
+
+  std::cout << "List size after 15 push backs: " << dat.size() << " (Expected: 15)" << std::endl;
+
+  return 0;
+
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,125 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+// pushback
+void ULListStr::push_back(const std::string&val){
+  if(empty()){
+    Item* newItem = new Item();
+    head_ = tail_ = newItem;
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+  } 
+  else if (tail_->last == ARRSIZE){
+    Item* newItem = new Item();
+    tail_->next = newItem;
+    newItem->prev = tail_;
+    tail_= newItem;
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+  }
+  else{
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+  }
+  size_++;
+}
+
+// pushfront
+void ULListStr::push_front(const std::string&val){
+  if(empty()){
+    Item* newItem = new Item();
+    head_ = tail_ = newItem;
+    newItem->val[ARRSIZE-1] = val;
+    newItem->first = ARRSIZE-1;
+    newItem->last = ARRSIZE;
+  } 
+  else if (head_->first == 0){
+    Item* newItem = new Item();
+    newItem->next = head_;
+    head_->prev = newItem;
+    head_= newItem;
+    
+    newItem->val[ARRSIZE-1] = val;
+    newItem->first = ARRSIZE-1;
+    newItem->last = ARRSIZE;
+  }
+  else{
+    head_->first--;
+    head_->val[head_->first] = val;
+  }
+  size_++;
+}
+
+// pop back
+void ULListStr::pop_back(){
+  if (empty()) return;
+
+  tail_->last--;
+  size_--;
+
+  if(tail_->first == tail_->last){
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    
+    if (tail_ != NULL) {
+      tail_->next = NULL;
+    } else {
+      head_ = NULL;
+    }
+    delete temp;
+  }
+}
+
+// pop front
+void ULListStr::pop_front() {
+  if (empty()) return;
+
+  head_->first++;
+  size_--;
+
+  if (head_->first == head_->last) {
+    Item* temp = head_;
+    head_ = head_->next;
+    
+    if (head_ != NULL) {
+      head_->prev = NULL;
+    } else {
+      tail_ = NULL; 
+    }
+    delete temp;
+  }
+}
+
+// return functions
+std::string const & ULListStr::back() const {
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const {
+  return head_->val[head_->first];
+}
+
+// getValAtLoc
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  if (loc>=size_){
+    return NULL;
+  }
+
+  Item* current = head_;
+
+  while(current != NULL){
+    size_t numItemsInNode = current->last - current->first;
+    if(loc < numItemsInNode){
+      return &current->val[current->first +loc];
+    }
+
+    loc = loc - numItemsInNode;
+    current = current->next;
+  }
+  return NULL;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
